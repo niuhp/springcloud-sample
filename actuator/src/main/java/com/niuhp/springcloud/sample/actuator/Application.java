dac3fb3e2462ace0589548609069e228a7a465dc
niuhp <niuhp_dev@126.com>
2017-04-10 15:14:55 +0800

    actuator示例

---
+++ actuator/src/main/java/com/niuhp/springcloud/sample/actuator/Application.java
@@ -0,0 +1,14 @@
+package com.niuhp.springcloud.sample.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by niuhaipeng on 2017/3/15.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
