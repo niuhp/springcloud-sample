4c9335d982a2e8311a06f58741a44abd2b9e751f
niuhp <niuhp_dev@126.com>
2018-04-10 10:49:52 +0800

    Eureka 集群

---
+++ eureka-server/src/main/java/com/niuhp/springcloud/sample/eureka/server/EurekaServer1.java
@@ -0,0 +1,16 @@
+package com.niuhp.springcloud.sample.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by niuhaipeng on 2017/3/15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer1 {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServer1.class, "--spring.profiles.active=peer1");
+  }
+}
